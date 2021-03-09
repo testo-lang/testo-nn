f64f7a6546d30cde5a566297464830cc398a3050
Sergey Lvov <log0div0@gmail.com>
2021-03-09 12:14:29 +0300

    поиск слова HEISENBUG

--- example_selenium.py
+++ example_selenium.py
@@ -3,7 +3,7 @@ from selenium import webdriver
 from text_stuff import image_contains_text
 
 driver = webdriver.Chrome()
-driver.get("https://yandex.ru/")
+driver.get("https://heisenbug-piter.ru/")
 image = driver.get_screenshot_as_png()
-assert image_contains_text(image, "Яндекс")
+assert image_contains_text(image, "HEISENBUG")
 driver.close()
